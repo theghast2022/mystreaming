94b44b0dd1b6b5130c29cda11745a171a3b51206
theghast2022 <44522450+theghast2022@users.noreply.github.com>
2018-11-23 19:52:41 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ client.on('message', message => {
       message.channel.send(`**✅   ${argresult}**`)
   } else 
   if (message.content.startsWith(adminprefix + 'sets')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/dream");
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
       message.channel.send(`**✅**`)
   }
   if (message.content.startsWith(adminprefix + 'setname')) {
